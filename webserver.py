7df688de2100b756a824415bca05740e067d9e63
James Tate II <jamesbtate@gmail.com>
2019-04-25 13:34:00 -0400

    Made basic webserver program: webserver.py

---
+++ webserver.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Webserver for this application...
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
