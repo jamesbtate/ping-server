f8166c594221014cbe0aefc941f602416ef3f66d
James Tate II <james@tate2.com>
2020-02-08 21:52:51 -0500

    made containers automatically restart

--- docker_create.bash
+++ docker_create.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker create --name ping_influxdb -h influxdb --network ping -p 8086:8086 -v /opt/ping/influxdb/:/var/lib/influxdb influxdb
-docker create --name ping_mariadb -h mariadb --network ping -p 13306:3306 -v /opt/ping/mariadb/:/var/lib/mysql \
+docker create --name ping_influxdb --restart always -h influxdb --network ping -p 8086:8086 -v /opt/ping/influxdb/:/var/lib/influxdb influxdb
+docker create --name ping_mariadb --restart always -h mariadb --network ping -p 13306:3306 -v /opt/ping/mariadb/:/var/lib/mysql \
     -e MYSQL_ROOT_PASSWORD=password \
     -e MYSQL_DATABASE=ping \
     -e MYSQL_USER=ping \
@@ -8,6 +8,6 @@ docker create --name ping_mariadb -h mariadb --network ping -p 13306:3306 -v /op
     -v $PWD/schema.sql:/docker-entrypoint-initdb.d/schema.sql \
     mariadb:10.3
 
-docker create --name ping_collector -h collector --network ping -p 8765:8765 ping:collector
-docker create --name ping_web -h web --network ping -p 5000:5000 ping:web
-docker create --name ping_probe -h probe --network ping ping:probe
+docker create --name ping_collector --restart always -h collector --network ping -p 8765:8765 ping:collector
+docker create --name ping_web --restart always -h web --network ping -p 5000:5000 ping:web
+docker create --name ping_probe --restart always -h probe --network ping ping:probe
