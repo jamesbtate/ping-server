890534428ac39fd72c1207dff3799e627ef5d457
James Tate II <james@tate2.com>
2020-03-05 22:49:57 -0500

    slight tweaks to schema to tag it as version 0

--- schema.sql
+++ schema.sql
@@ -1,8 +1,9 @@
-USE `ping`;
-CREATE TABLE `src_dst` (
+CREATE TABLE `binary_src_dst` (
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `src` int(10) unsigned NOT NULL,
   `dst` int(10) unsigned NOT NULL,
+  `binary_file` varchar(256) DEFAULT NULL,
+  `max_records` bigint(20) unsigned NOT NULL DEFAULT '604800',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB COLLATE='utf8_general_ci';
 CREATE TABLE `output` (
@@ -12,12 +13,9 @@ CREATE TABLE `output` (
   KEY `src_dst` (`src_dst`),
   CONSTRAINT `output_ibfk_1` FOREIGN KEY (`src_dst`) REFERENCES `src_dst` (`id`)
 ) ENGINE=InnoDB COLLATE='utf8_general_ci';
-
-CREATE TABLE `binary_src_dst` (
+CREATE TABLE `src_dst` (
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `src` int(10) unsigned NOT NULL,
   `dst` int(10) unsigned NOT NULL,
-  `binary_file` varchar(256) NULL,
-  `max_records` bigint(20) unsigned NOT NULL DEFAULT '604800',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB COLLATE='utf8_general_ci';
