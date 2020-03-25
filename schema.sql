de960a5e3b56082d29eaf409c5a5986d201e4144
James Tate II <james@tate2.com>
2020-03-24 20:24:03 -0400

    corrected schema version 3 (schema.sql was not updated but mysql_upgrades.py was)

--- schema.sql
+++ schema.sql
@@ -10,6 +10,33 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `prober` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `added` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `prober_target` (
+  `id` bigint(20) NOT NULL,
+  `prober_id` bigint(20) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ip` int(10) unsigned NOT NULL,
+  `type` enum('icmp','udp','tcp') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `port` int(10) unsigned DEFAULT NULL,
+  `added` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `prober_id` (`prober_id`),
+  CONSTRAINT `prober_target_ibfk_1` FOREIGN KEY (`prober_id`) REFERENCES `prober` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `src_dst` (
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `src` int(10) unsigned NOT NULL,
@@ -33,4 +60,4 @@ CREATE TABLE `version` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 DELETE FROM version;
-INSERT INTO `version` VALUES (2);
+INSERT INTO `version` VALUES (3);
